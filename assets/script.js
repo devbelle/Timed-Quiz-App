b80940a3c38e864b698f23a568c260c5a319997d
Devin <dbelle722@gmail.com>
2023-12-10 16:52:04 -0500

    starting files

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+//Call out your variables
+    //questions
+    //answer selections
+    //timer
+    //score
+//create functions
+    //timer function
+    //start game
+    //next question
+//need an array of questions
+//need score counter
+    //save score in local storage
+//need timer counter
+    //decrease timer when answering wrong
