2f9fc102fded9a07f4eb0a013601009a5f6a69af
Devin <dbelle722@gmail.com>
2023-12-11 17:52:23 -0500

    Final Comments

--- assets/end.js
+++ assets/end.js
@@ -1,8 +1,6 @@
-
-
 //links save socre button from html doc 
 const submitBtn = document.getElementById('submit');
-//links the final score
+//links the final score with mostRecentScore in form
 const finalScore = document.getElementById('finalScore');
 //retrieves the end score from local storage
 const mostRecentScore = localStorage.getItem('mostRecentScore');
@@ -10,18 +8,19 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 //Links the form to the end page
 const form = document.querySelector('form');
 
-//parsing high scores to turn array into string values. If no score, bring an empty array to get rid of null value
-
-
 //pulls most recent score from game script and saves it to end screen
 finalScore.innerText = mostRecentScore;
 
-
+//create an add even listener to save the initials at the end
 form.addEventListener('submit', (e) => {
+    //keeps initials from being put inot the URL (default action)
     e.preventDefault();
-    const username = new FormData(form);
-    const obj = Object.fromEntries(username);
+    //created a variable to gather all input in the form into a data object.
+    const initials = new FormData(form);
+    //created a variable for the object 
+    const obj = Object.fromEntries(initials);
     
+    //used JSON stringify to save the object as a string into local storage
     const json = JSON.stringify(obj);
     localStorage.setItem('form', json);
 
