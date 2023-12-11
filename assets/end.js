3393a6252e557bdff95dc28b5ff5aa49fa248c7b
Devin <dbelle722@gmail.com>
2023-12-11 17:43:40 -0500

    Saving Initials Added

--- assets/end.js
+++ assets/end.js
@@ -1,29 +1,35 @@
-//links username from end html doc
-const username = document.getElementById('username');
-//links save socre button from html doc
-const saveScoreBtn = document.getElementById('saveScoreBtn');
+
+
+//links save socre button from html doc 
+const submitBtn = document.getElementById('submit');
 //links the final score
 const finalScore = document.getElementById('finalScore');
 //retrieves the end score from local storage
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+//Links the form to the end page
+const form = document.querySelector('form');
+
 //parsing high scores to turn array into string values. If no score, bring an empty array to get rid of null value
-const newScores = JSON.parse(localStorage.getItem("newScores"));
-console.log(newScores);
 
 
 //pulls most recent score from game script and saves it to end screen
 finalScore.innerText = mostRecentScore;
 
-username.addEventListener('keyup', () => {
-    //disable if there is no username input so you can't click it.
-    saveScoreBtn.disabled = !username.value;
-    //turn array entry into a string
-    localStorage.setItem('username', username);
 
-});
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const username = new FormData(form);
+    const obj = Object.fromEntries(username);
+    
+    const json = JSON.stringify(obj);
+    localStorage.setItem('form', json);
 
-//
-function saveUsername() {
-    JSON.parse(localStorage.getItem("username"));
-}
+    
+    
+})
+
+
+
+
+
